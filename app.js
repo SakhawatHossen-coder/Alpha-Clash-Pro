74dd770ec27f510cf92b09f0c6cf9ef6ff5375cd
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-02-14 23:02:46 +0600

    some features add

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const screenSection = document.getElementById("screen-section");
 const playgroundSection = document.querySelector("#playground-section");
+const resultSection = document.querySelector("#result-section");
 const alphaBtn = document.getElementById("alpha-btn");
 
 alphaBtn.addEventListener("click", function () {
@@ -7,6 +8,42 @@ alphaBtn.addEventListener("click", function () {
   screenSection.classList.add("hidden");
 
   continueGame();
+});
+
+// keyboard Handler
+document.addEventListener("keyup", (e) => {
+  let gamerPressed = e.key;
+
+  //get the expected key
+  const currentAlphabet = document.querySelector("#current-letter");
+  const currentLetter = currentAlphabet.innerText.toLowerCase();
+  console.log(gamerPressed, currentLetter);
+
+  //checking to match the show and pressed letters
+  if (gamerPressed === currentLetter) {
+    //get the score
+    let scoreElement = document.querySelector("#game-score");
+    const scoreNumberText = scoreElement.innerText;
+    const scoreNumber = parseInt(scoreNumberText);
+    let newScore = scoreNumber + 1;
+    //update the score
+    scoreElement.innerText = newScore;
+
+    removeBg(currentLetter);
+    // increasingScore();
+    continueGame();
+  } else {
+    //get the life
+    const gameLife = document.querySelector("#game-life");
+    const gameLifeText = gameLife.innerText;
+    const gameLifeNumber = parseInt(gameLifeText);
+    let newLife = gameLifeNumber - 1;
+    gameLife.innerText = newLife;
+if(newLife===0){
+  gameOver()
+}
+
+  }
 });
 
 function randomAlphabet() {
@@ -25,7 +62,33 @@ function continueGame() {
   setBg(alphabet);
 }
 
+function gameOver(){
+
+playgroundSection.classList.add("hidden");
+resultSection.classList.remove("hidden")
+
+}
 function setBg(elementId) {
   const element = document.getElementById(elementId);
   element.classList.add("bg-orange-400");
 }
+
+const removeBg = (elementId) => {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-orange-400");
+};
+
+// increasing score
+/*
+const increasingScore = () => {
+  // let score = 0;
+  const scoreElement = document.querySelector("#game-score").innerText;
+  const scoreNumber = parseInt(scoreElement);
+  console.log(scoreNumber);
+  const newScore = scoreNumber + 1;
+  //update
+  return (scoreElement.innerText = newScore);
+
+  return newScore;
+};
+*/
